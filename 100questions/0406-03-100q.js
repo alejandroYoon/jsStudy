f4edf75c64c80a5759438ec984b5891d9c60204a
alejandroYoon <datadog@naver.com>
2023-04-06 22:20:35 +0900

    100제 3번문제 - typeof 의 사용법

---
+++ 100questions/0406-03-100q.js
@@ -0,0 +1,27 @@
+/* 문제 3.
+
+# 문제3 : 변수의 타입
+
+다음 출력 값으로 올바른 것은?
+
+var arr = [100, 200, 300];
+console.log(typeof(arr));
+
+1)  undefined
+2)  string
+3)  number
+4)  object
+
+*/
+
+
+
+/* 정답 
+4. object
+
+typeof 는 대상의 타입을 정의해주는 함수이다.
+
+사용법 "typeof 단어" 로 해당 단어의 타입을 출력할 수 있다.
+예를들어, 문자는 string, 숫자는 number, true와 false 는 boolean, null과 배열은 object가 나온다.  
+
+*/
