03223158a48563d8abe2fcfa62be01761ee09ab3
alejandroYoon <datadog@naver.com>
2023-04-08 22:48:18 +0900

    100제 9번 풀이 업로드

---
+++ 100questions/0408-09-100q.js
@@ -0,0 +1,19 @@
+/*
+
+다음 소스 코드를 완성하여 날짜와 시간을 출력하시오.
+
+*/
+
+// 데이터
+const year = '2019';
+const month = '04';
+const day = '26';
+const hour = '11';
+const minute = '34';
+const second = '27';
+let result = year.concat('/',month,'/',day,' ',hour,':',minute,':',second);
+console.log(result);
+
+
+// 출력
+// 2019/04/26 11:34:27
