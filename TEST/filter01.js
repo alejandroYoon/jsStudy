4562071895879935d6ab463531c4f1757630e887
alejandroYoon <datadog@naver.com>
2023-04-12 02:23:42 +0900

    filter 테스트

---
+++ TEST/filter01.js
@@ -0,0 +1,14 @@
+let array = [3,5,11,0,9,'string'];
+
+// solution 1
+
+let a = array.filter((value) => value < 6)
+    console.log(a)
+
+// solution 2
+function less10 (value){
+  return value < 10
+}
+
+let result = array.filter(less10)
+console.log(result)
