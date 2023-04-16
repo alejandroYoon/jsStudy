5b0a6c2882ca96b336b864152e79d0b99049352c
alejandroYoon <datadog@naver.com>
2023-04-17 02:17:20 +0900

    0403 array

--- basic/0403-문제_Array.js
+++ basic/0403-문제_Array.js
@@ -29,50 +29,65 @@ console.log(" 5. ----------- 문다은 ")
 
   // for 사용
   let input_data4 = [1, 2, 3, 4, 5];
-  console.log(input_data); // [11,12,13,14,15]
+  console.log(input_data4); // [11,12,13,14,15]
 
 
   console.log(" 8. ----------- 배상아 ")
 
   // forEach 사용
   let input_data5 = [10, 20, 30, 40, 50];
-  console.log(input_data); // [5, 10, 15, 20, 25]
+  console.log(input_data5); // [5, 10, 15, 20, 25]
 
 
   console.log(" 9. ----------- 윤진섭 ")
 
   // map 사용
+
+  // let input_data6 = [10, 20, 30, 40, 50];
+  // input_data6.splice(0,1,1,2,3);
+  // input_data6.splice(4,3,25);
+  // output_data6 = input_data6
+  // console.log(output_data6); // [1, 2, 3, 20, 25]
+
+
   let input_data6 = [10, 20, 30, 40, 50];
-  console.log(output_data); // [1, 2, 3, 20, 25]
+  input_data6.splice(0,1,1,2,3);
+  output_data6 = input_data6.splice(4,3,25).map((num) => num[0];
+  console.log(output_data6); // [1, 2, 3, 20, 25]
 
 
+
+
+
+
+  
   console.log(" 10. ----------- 문다은 ")
 
   let input_data7 = [10, 20, 30, 40, 50];
-  console.log(output_data); // [30, 40]
+  console.log(output_data7); // [30, 40]
 
 
   console.log(" 11. ----------- 배상아 ")
 
   let input_data8 = [10, 20, 30, 40, 50];
-  console.log(input_data); // [10, 20, 50]
+  console.log(input_data8); // [10, 20, 50]
 
 
   console.log(" 12. ----------- 윤진섭 ")
 
   let input_data9 = [10, 20, 30, 40, 50];
-  console.log(output_data); // 3 *인덱스 번호
+  console.log(output_data9); // 3 *인덱스 번호
 
 
   console.log(" 13. ----------- 문다은 ")
 
   let input_data10 = "오늘 날씨는 매우 맑음";
-  console.log(output_data); // ["오늘", "날씨는", "매우", "흐림"]
+  console.log(output_data10); // ["오늘", "날씨는", "매우", "흐림"]
 
 
   console.log(" 14. ----------- 배상아 ")
 
   // '=' 금지
   let input_data = "오늘 날씨는 매우 맑음";
-  console.log(output_data); // ["내일", "날씨는", "매우", "맑음]
+  console.log(output_data11); // ["내일", "날씨는", "매우", "맑음]
 
