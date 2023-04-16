f323250472d49b90c67ba57759e3aeeb3cf82f9d
alejandroYoon <datadog@naver.com>
2023-04-17 01:29:32 +0900

    0403-object 문제 다 품

--- basic/0403-문제_Object.js
+++ basic/0403-문제_Object.js
@@ -1,6 +1,5 @@
 
 
-// 윤진섭
 // 객체 선언
 // 나머지 문제들 전부 해당 객체를 복사 붙여넣기해서 사용합니다
 
@@ -8,10 +7,19 @@
 // 각각의 id와 name의 값으로 '웹페', '이지' ,'단순' 을 가지고 있는 객체를 만드시오
 
 console.log(" ----------- 객체선언 ")
-obj1 = [
-    {name : "웹페"},
-    {name : "이지"},
-    {name : "단순"},
+let obj1 = [
+    {
+      id : 1,
+      name : "웹페"
+    },
+    {
+      id : 2,
+      name : "이지"
+    },
+    {
+      id : 3,
+      name : "단순"
+    },
 ]
 
 // {
@@ -19,21 +27,39 @@ obj1 = [
 //   console.log(output_data); // 이지
 // }
 
-// 문다은
+// 
 //   객체 접근
-console.log(" ----------- 1")
-// let output_data = ojb1[1]
+console.log(" 윤진섭 ----------- 1-1")
 console.log(obj1[1].name); // 이지
 
-// 배상아
+console.log(" 윤진섭 ----------- 1-2")
+const output_data = obj1[1];
+console.log(output_data);
+
+console.log(" 윤진섭 ----------- 1-3")
+const output_data2 = obj1[1].name;
+console.log(output_data2);
+
+
+console.log(" 문다은 ----------- 2")
 //   객체 값 변경
 //   2번째 id의 값을 '이퍼'로 변경하시오
+// 
+// obj1[1].id="이퍼"
+// console.log(obj1[1]);
 
-// 윤진섭
+
+console.log(" 배상아 ----------- 3")
 //   객체 찾기
 //   3번째 단순만 Key, value를 둘 다 출력하시오
+// console.log(obj1[2])
+// console.log(obj1[obj1.length - 1])
+// console.log(obj1[obj1.length - 1].name)
 
-// 문다은
-//   const array = [1,2,3,4,5]
+console.log(" 윤진섭 ----------- 4")
+   const array = [1,2,3,4,5]
 //   위의 배열의 값을 가지고 객체의 '단순'을 출력하시오
-
+const result = array[1];
+console.log(result);
+console.log(obj1[2].name);
+console.log(obj1[result].name);
