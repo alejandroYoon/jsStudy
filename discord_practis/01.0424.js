172a24451c727970ec048e759579b9fdf88fbf4b
yoonjs <yoonjs@pittasoft.com>
2023-04-25 18:01:06 +0900

    0424 문제풀이 복습하기

---
+++ discord_practis/01.0424.js
@@ -0,0 +1,212 @@
+const students = [
+    {
+      id: 1,
+      name: "김철수",
+      age: 18,
+      gender: "male",
+      grade: 3,
+      class: 1,
+      scores: {
+        math: 85,
+        english: 92,
+        science: 78,
+      },
+    },
+    {
+      id: 2,
+      name: "이영희",
+      age: 17,
+      gender: "female",
+      grade: 3,
+      class: 2,
+      scores: {
+        math: 78,
+        english: 95,
+        science: 87,
+      },
+    },
+    {
+      id: 3,
+      name: "박민수",
+      age: 16,
+      gender: "male",
+      grade: 2,
+      class: 1,
+      scores: {
+        math: 92,
+        english: 88,
+        science: 90,
+      },
+    },
+    {
+      id: 4,
+      name: "정미영",
+      age: 18,
+      gender: "female",
+      grade: 3,
+      class: 3,
+      scores: {
+        math: 90,
+        english: 82,
+        science: 93,
+      },
+    },
+    {
+      id: 5,
+      name: "이영수",
+      age: 17,
+      gender: "male",
+      grade: 2,
+      class: 2,
+      scores: {
+        math: 80,
+        english: 85,
+        science: 88,
+      },
+    },
+    {
+      id: 6,
+      name: "한지민",
+      age: 18,
+      gender: "female",
+      grade: 3,
+      class: 1,
+      scores: {
+        math: 95,
+        english: 92,
+        science: 96,
+      },
+    },
+    {
+      id: 7,
+      name: "신동엽",
+      age: 16,
+      gender: "male",
+      grade: 2,
+      class: 3,
+      scores: {
+        math: 87,
+        english: 89,
+        science: 91,
+      },
+    },
+    {
+      id: 8,
+      name: "최지우",
+      age: 17,
+      gender: "female",
+      grade: 2,
+      class: 2,
+      scores: {
+        math: 92,
+        english: 87,
+        science: 85,
+      },
+    },
+    {
+      id: 9,
+      name: "장우혁",
+      age: 16,
+      gender: "male",
+      grade: 3,
+      class: 3,
+      scores: {
+        math: 88,
+        english: 84,
+        science: 90,
+      },
+    },
+    {
+      id: 10,
+      name: "홍길동",
+      age: 18,
+      gender: "male",
+      grade: 3,
+      class: 2,
+      scores: {
+        math: 85,
+        english: 90,
+        science: 92,
+      },
+    },
+  ];
+  
+  // 모든 문제들은 배열 혹은 변수에 담아서 출력해주세요.
+  // 배열 없이 console.log()를 사용 할 경우 재사용을 할 수가 없습니다.
+  
+  // 0. 학생들이 총 몇명이 있는지 출력해주세요.
+  console.log(" 문제 0 -------------------")
+  let cnt_students = students.length
+  console.log (`학생들의 숫자는 ${cnt_students}명 입니다.`)
+
+
+  // 1. 나이가 17살 미만인 학생만 출력해주세요. (이름만 출력하기)
+  console.log()
+  console.log(" 문제 1 -------------------")
+  let under17 = students.filter(user => user.age < 17).map(user => user.name,)
+  console.log(under17)
+
+
+
+  // 2. 2반인 학생들만 출력해주세요
+  console.log()
+  console.log(" 문제 2 ------------------- 풀이1")
+  let class2 = students.filter(user => user.class = 2)
+  class2.forEach(user => console.log(`${user.name}`, `- ${user.class}반`))
+
+  console.log(" 문제 2 ------------------- 풀이2")
+  let class22 = students.filter(user => user.class = 2).map(user => ([user.name, user.age]))
+  console.log(class22)
+
+
+  // 3. 성별이 "male"인 학생들만 출력해주세요
+  console.log()
+  console.log(" 문제 3 ------------------- 풀이1")
+  let genderMan = students.filter(user => user.gender === "male")
+  console.log(genderMan)
+
+  console.log(" 문제 3 ------------------- 풀이2")
+  for (let i=0; i < students.length; i++){
+    if (students[i].gender === "male"){
+  console.log(`${students[i].name}, ${students[i].age}살`)
+    }
+  }
+
+
+  // 4. 각 학생들을 "제 이름은 @@@ 이고 나이는 @@ 입니다." 형식에 맞게 출력해주세요
+  console.log()
+  console.log(" 문제 4 -------------------")
+  for (i=0; i < students.length; i++){
+    console.log(`${i}. 제 이름은 ${students[i].name}이고, 나이는 ${students[i].age} 입니다.`)
+  }
+
+
+
+  // 5. 각 학생들의 수학, 영어의 점수를 모두 더해주고 평균을 구해주세요 (수학 + 영어) / 학생 수
+  console.log()
+  console.log(" 문제 5 -------------------")
+//   let avgMathEng = students.map(user => (user.scores.math / user.length));
+    // console.log(avgMathEng)
+
+let avgMathEng = students.reduce((acumulator, current) => {
+    return acumulator + current.age;
+}, 0);
+console.log(`(수학 + 영어) / 학생수 = ${avgMathEng}`)
+
+// let avgMathEng = students.reduce((accumulator, current) => accumulator + current.age);
+// console.log(`sum1 = `, avgMathEng)
+
+
+
+
+
+  // 6. 2반 학생들의 수학 점수의 평균을 구해주세요
+  console.log()
+  console.log(" 문제 6 -------------------")
+
+  let class2Math = students.filter(user => user.class === 2).map(user => {
+  math.students (sum+=user.scores.math);
+  console.log(user.scores.math)
+//   return class2MathSum;
+})
+console.log (class2Math)
