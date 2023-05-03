d4ef0652b18937c99f48b5261bd5281af2743b8f
jsyoon <jsyoon@bsgglobal.com>
2023-05-04 02:04:24 +0900

    useState 리액트 훅

--- React_Study/별코딩/20230503_statehook/src/App.js
+++ React_Study/별코딩/20230503_statehook/src/App.js
@@ -1,26 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
 
+const heavyWork = () => {
+  console.log('엄청 무거운 작업 !!!');
+  return ['홍길동','김민수','이순신'];
+};
 
 function App() {
- const [time, setTime] = useState(1);
+  const [names, setName] = useState(() => {
+    return heavyWork();
+  });
+  const [input, setInput] = useState('');
 
- const handleClick = () => {
-  //  let newTime;
-   if(time >= 24) {
-     newTime=1;
-   }else {
-     newTime= time +1;
-   }
-   setTime(newTime);
- };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  };
+
+const handleUpload = () => {
+  setName((prevState) => {
+    console.log('이전 state:', prevState);
+    return[input, ...prevState];
+  });
+};
 
 
   return (
     <div>
-      <span>현재 시각: {time}시</span> <br></br>
-      <button onClick= {handleClick}>Update</button>
+      <input type="text" value={input} onChange={handleInputChange}/>
+        <button onClick = {handleUpload}> Upload</button>
+        {names.map ((name, idx) => {
+          return <p key={idx}>{name}</p>;
+        })}
     </div>
   );
 }
