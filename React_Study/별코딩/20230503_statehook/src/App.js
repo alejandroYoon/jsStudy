ee055076e53fc68f3f2b2ff2292178fafb0c0b5d
jsyoon <jsyoon@bsgglobal.com>
2023-05-04 01:05:10 +0900

    별코딩 업데이트

--- React_Study/별코딩/20230503_statehook/src/App.js
+++ React_Study/별코딩/20230503_statehook/src/App.js
@@ -6,15 +6,20 @@ import { useState } from 'react';
 function App() {
  const [time, setTime] = useState(1);
 
+ const handleClick = () => {
+  //  let newTime;
+   if(time >= 24) {
+     newTime=1;
+   }else {
+     newTime= time +1;
+   }
+   setTime(newTime);
+ };
 
-
-  const handleClick = () => {
-    setTime(time + 1);
-  };
 
   return (
     <div>
-      <span>현재 시각: {time}시</span>
+      <span>현재 시각: {time}시</span> <br></br>
       <button onClick= {handleClick}>Update</button>
     </div>
   );
