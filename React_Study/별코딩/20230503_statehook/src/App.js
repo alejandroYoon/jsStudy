a964cdd5b171bb432e98f4ff95d1e32f9f9f5573
jsyoon <jsyoon@bsgglobal.com>
2023-05-04 00:43:12 +0900

    별코딩 hook 예제1번 업데이트

---
+++ React_Study/별코딩/20230503_statehook/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+ const [time, setTime] = useState(1);
+
+
+
+  const handleClick = () => {
+    setTime(time + 1);
+  };
+
+  return (
+    <div>
+      <span>현재 시각: {time}시</span>
+      <button onClick= {handleClick}>Update</button>
+    </div>
+  );
+}
+
+export default App;
