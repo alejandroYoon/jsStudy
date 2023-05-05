3da67331ad958ca5d82e1bae0cd74332524baa5e
jsyoon <jsyoon@bsgglobal.com>
2023-05-05 23:36:16 +0900

    훅함수 테스트 useState

---
+++ React_Study/별코딩/20230505_useref/src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const State = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment123</button>
+    </div>
+  );
+};
+
+export default State
