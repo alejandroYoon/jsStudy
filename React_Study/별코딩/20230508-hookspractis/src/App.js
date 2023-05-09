2624fe72338f03294361539d419d58a0f09104cf
jsyoon <jsyoon@bsgglobal.com>
2023-05-09 23:58:08 +0900

    useMemo test

--- React_Study/별코딩/20230508-hookspractis/src/App.js
+++ React_Study/별코딩/20230508-hookspractis/src/App.js
@@ -1,35 +1,25 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 
-const heavyWork = () => {
-  console.log('엄청 무거운 작업 !!!');
-  return ['홍길동','김민수'];
+const hardCalculate = (number) => {
+  console.log('어려운 계산기');
+  for (let i=0; i < 999999999; i++) {} //생각하는 시간
+  return number + 1000;
 };
 
 function App() {
-  const [names, setNames] = useState(() => {
-    return heavyWork();
-  });
-  const [input, setInput ] = useState('');
+  const [hardNumber, setHardNumber] = useState(1);
+  const hardSum = hardCalculate(hardNumber);
 
-  const handleInputChange = (e) => {
-    setInput(e.target.value);
-  };
-
-  const handleUpload = () => {
-    setNames((prevState) => {
-      console.log('이전 state: ', prevState);
-      return [input, ...prevState];
-    });
-  };
-
-  return (
-    <div>
-      <input type="text" value={input} onChnage={handleInputChange}/>
-      <button></button>Upload</tton>
-      {names.map((name,idx) => {
-        return <p key={idx}>{name}</p>;
-      })}
-    </div>
+return (
+  <div>
+    <h3>어려운 계산기</h3>
+    <input
+      type="number"
+      value={hardNumber}
+      onChange={(e) => setHardNumber(parseInt(e.target.value))}
+      />
+      <span> + 1000 = {hardSum}</span>
+  </div>
   );
 }
 
